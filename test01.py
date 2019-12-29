ae6c2abd7c4c19a38ff3a15043acd9c16a739de3
Oliver Chen <oliverchenbing@126.com>
2019-12-29 15:03:06 +0800

    commit 01

---
+++ test01.py
@@ -0,0 +1,14 @@
+vegetable = ["青菜", "萝卜", "花菜"]
+print(id(vegetable))
+
+new_list_01 = vegetable.copy()
+print(id(new_list_01))
+
+
+new_list_02 = vegetable
+print(id(new_list_02))
+
+vegetable.append("卷心菜")
+
+print(vegetable, new_list_01)
+print(vegetable, new_list_02)
