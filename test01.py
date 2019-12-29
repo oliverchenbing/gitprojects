86a29711b6586fff0e2a4a0044e9c95e208315d6
Oliver Chen <oliverchenbing@126.com>
2019-12-29 15:32:40 +0800

    commit 03

--- test01.py
+++ test01.py
@@ -1,4 +1,6 @@
-﻿vegetable = ["青菜", "萝卜", "花菜"]
+﻿# _*_ coding: utf-8 _*_
+
+vegetable = ["青菜", "萝卜", "花菜"]
 print(id(vegetable))
 
 new_list_01 = vegetable.copy()
@@ -14,3 +16,6 @@ print(vegetable, new_list_01)
 print(vegetable, new_list_02)
 
 print("hello world")
+
+def test():
+    pass
